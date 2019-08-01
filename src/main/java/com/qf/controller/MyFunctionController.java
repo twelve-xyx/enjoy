3ece74bb4a410686d973eb8a54da92ad7c6d8b2a
xyhyhx <dtxyh95309@163.com>
2019-08-01 15:29:54 +0800

    我的功能

---
+++ src/main/java/com/qf/controller/MyFunctionController.java
@@ -0,0 +1,50 @@
+package com.qf.controller;
+
+import com.qf.pojo.Dynamic;
+import com.qf.pojo.Friend;
+import com.qf.pojo.Users;
+import com.qf.service.UsersService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MyFunctionController {
+    @Resource
+    private UsersService usersServicel;
+    @RequestMapping("/user/userinformation")
+    public Users myInfo(Users users){
+        return usersServicel.getUsers(users);
+    }
+
+
+
+    @RequestMapping("/user/myDynamic")
+    public List<Dynamic> myDynamic(Dynamic dynamic){
+        return usersServicel.getDynamic(dynamic);
+    }
+
+
+    @RequestMapping("/user/userDynamic")
+    public List<Dynamic> userDynamic(Dynamic dynamic)
+    {
+        return usersServicel.getDynamic(dynamic);
+    }
+
+    @RequestMapping("/user/rvisitor")
+    public List<Users> getRecentVisitor(Friend friend){
+      List<Friend> friends=new ArrayList<>();
+        friends=  usersServicel.findFriend(friend);
+        List list=new ArrayList();
+        for (Friend friend1 : friends) {
+            list.add(friend1.getU2id());
+        }
+        List<Users> users=usersServicel.findFriendMsg(list);
+        return users;
+
+
+    }
+}
