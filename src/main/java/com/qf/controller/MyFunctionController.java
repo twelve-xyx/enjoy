01280e4e9dcf5a91f697694c8f061de5837cd4c9
xyhyhx <dtxyh95309@163.com>
2019-08-10 16:27:17 +0800

    Merge branch 'master' of https://github.com/twelve-xyx/enjoy
    # Conflicts:
    #	.idea/workspace.xml
    #	src/main/java/com/qf/controller/DynamicController.java
    #	target/classes/com/qf/dao/UsersMapper.class
    #	target/classes/com/qf/service/Impl/UsersServiceImpl.class
    #	target/classes/com/qf/service/UsersService.class
    #	target/classes/mapper/UsersMapper.xml
    #	target/exam/WEB-INF/classes/com/qf/dao/UsersMapper.class
    #	target/exam/WEB-INF/classes/com/qf/service/Impl/UsersServiceImpl.class
    #	target/exam/WEB-INF/classes/com/qf/service/UsersService.class
    #	target/exam/WEB-INF/classes/mapper/UsersMapper.xml

--- src/main/java/com/qf/controller/MyFunctionController.java
+++ src/main/java/com/qf/controller/MyFunctionController.java
@@ -66,7 +66,7 @@ public class MyFunctionController {
     }
 
     //收藏列表
-    @RequestMapping("/user/cellection")
+    @RequestMapping("/user/collection")
     public List<Collect> Collection(Collect collect){
       return collectService.findOnesCollect(collect);
     }
